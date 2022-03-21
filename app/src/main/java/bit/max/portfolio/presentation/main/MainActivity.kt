89bcb8f4bc561c557732d8f6e26eed55f70b43cb
maksim <maxim-bit@mail.ru>
2022-03-21 11:38:48 +0300

    Dummy main view model added

--- app/src/main/java/bit/max/portfolio/presentation/main/MainActivity.kt
+++ app/src/main/java/bit/max/portfolio/presentation/main/MainActivity.kt
@@ -1,16 +1,31 @@
 package bit.max.portfolio.presentation.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import bit.max.portfolio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.saveButton.setOnClickListener {
+            val firstName = binding.firstnameEditText.text.toString()
+            val lastName = binding.lastnameEditText.text.toString()
+
+            viewModel.saveUserName(firstName = firstName, lastName = lastName)
+        }
+
+
+        binding.showButton.setOnClickListener {
+            viewModel.fetchUserName()
+        }
     }
 }
---
+++ app/src/main/java/bit/max/portfolio/presentation/main/MainViewModel.kt
@@ -0,0 +1,15 @@
+package bit.max.portfolio.presentation.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+
+    fun saveUserName(firstName: String, lastName: String) {
+
+    }
+
+    fun fetchUserName() {
+
+    }
+}
