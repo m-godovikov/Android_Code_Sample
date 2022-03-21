62adb62236f09de4aafa7b2b4efc134563a79d4d
maksim <maxim-bit@mail.ru>
2022-03-21 10:42:09 +0300

    Initial

---
+++ app/src/main/java/bit/max/portfolio/MainActivity.kt
@@ -0,0 +1,11 @@
+package bit.max.portfolio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
